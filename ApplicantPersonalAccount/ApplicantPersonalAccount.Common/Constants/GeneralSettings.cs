86e8c5c551eef3f4ccd08e69e914aa91e456a8e1
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-03-29 14:01:12 +0700

    add hasher utility

---
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.Common/Constants/GeneralSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ApplicantPersonalAccount.Common.Constants
+{
+    public class GeneralSettings
+    {
+        public const int ACCESS_TOKEN_LIFETIME = 10; // in minutes
+    }
+}
---
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.Infrastructure/Utilities/Hasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Helpers;
+
+namespace ApplicantPersonalAccount.Infrastructure.Utilities
+{
+    public class Hasher
+    {
+        public string HashPassword(string password)
+        {
+            return Crypto.HashPassword(password);
+        }
+
+        public bool CheckPassword(string hashedPassword, string enteredPassword)
+        {
+            return Crypto.VerifyHashedPassword(hashedPassword, enteredPassword);
+        }
+    }
+}
