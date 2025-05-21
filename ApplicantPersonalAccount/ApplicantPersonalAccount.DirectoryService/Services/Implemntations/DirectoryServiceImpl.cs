cdba59a1577cc2a06dddaf33a8e51bff50e242c7
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-05-22 00:05:25 +0700

    add directory service

---
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.DirectoryService/Services/Implemntations/DirectoryServiceImpl.cs
@@ -0,0 +1,89 @@
+﻿using ApplicantPersonalAccount.Application.OuterServices.DTO;
+using ApplicantPersonalAccount.Common.Constants;
+using ApplicantPersonalAccount.Infrastructure.Converters;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+
+namespace ApplicantPersonalAccount.DirectoryService.Services.Implemntations
+{
+    public class DirectoryServiceImpl : IDirectoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DirectoryServiceImpl(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+            var bytes = Encoding.ASCII.GetBytes($"{ServerURLs.USERNAME}:{ServerURLs.PASSWORD}");
+            var credentials = Convert.ToBase64String(bytes);
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Basic", credentials);
+        }
+
+        public async Task<List<EducationLevel>> GetEducationLevels()
+        {
+            var response = await _httpClient.GetAsync(ServerURLs.EDUCATION_LEVELS);
+
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<List<EducationLevel>>(stringResponse, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonTimeConverter() }
+            });
+
+            return data ?? new List<EducationLevel>();
+        }
+
+        public async Task<List<DocumentType>> GetDocumentTypes()
+        {
+            var response = await _httpClient.GetAsync(ServerURLs.DOCUMENT_TYPES);
+
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<List<DocumentType>>(stringResponse, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonTimeConverter() }
+            });
+
+            return data ?? new List<DocumentType>();
+        }
+
+        public async Task<List<Faculty>> GetFaculties()
+        {
+            var response = await _httpClient.GetAsync(ServerURLs.FACULTIES);
+
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<List<Faculty>>(stringResponse, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonTimeConverter() }
+            });
+
+            return data ?? new List<Faculty>();
+        }
+
+        public async Task<ProgramPagedList> GetEducationPrograms(int page = 1, int size = 10)
+        {
+            var response = await _httpClient.GetAsync(ServerURLs.PROGRAMS + "?page=" + page + "&size=" + size);
+
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<ProgramPagedList>(stringResponse, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonTimeConverter() }
+            });
+
+            return data ?? new ProgramPagedList();
+        }
+    }
+}
--- ApplicantPersonalAccount/ApplicantPersonalAccount.DirectoryService/Setup/SetupServices.cs
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.DirectoryService/Setup/SetupServices.cs
@@ -1,10 +1,13 @@
-﻿namespace ApplicantPersonalAccount.DirectoryService.Setup
+﻿using ApplicantPersonalAccount.DirectoryService.Services;
+using ApplicantPersonalAccount.DirectoryService.Services.Implemntations;
+
+namespace ApplicantPersonalAccount.DirectoryService.Setup
 {
     public class SetupServices
     {
         public static void AddServices(IServiceCollection services)
         {
-            
+            services.AddHttpClient<IDirectoryService, DirectoryServiceImpl>();
         }
     }
 }
