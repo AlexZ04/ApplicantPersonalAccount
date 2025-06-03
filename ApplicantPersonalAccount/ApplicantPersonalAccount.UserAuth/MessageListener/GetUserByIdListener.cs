d5d90afffe8c0b0b82904152126b165f95ba1087
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-03 22:03:33 +0700

    fix getting user by id listener

--- ApplicantPersonalAccount/ApplicantPersonalAccount.UserAuth/MessageListener/GetUserByIdListener.cs
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.UserAuth/MessageListener/GetUserByIdListener.cs
@@ -1,4 +1,5 @@
-﻿using ApplicantPersonalAccount.Infrastructure.RabbitMq;
+﻿using ApplicantPersonalAccount.Common.DTOs;
+using ApplicantPersonalAccount.Infrastructure.RabbitMq;
 using ApplicantPersonalAccount.Infrastructure.RabbitMq.MessageListener;
 using ApplicantPersonalAccount.Persistence.Repositories;
 using RabbitMQ.Client.Events;
@@ -6,19 +7,19 @@ using System.Text.Json;
 
 namespace ApplicantPersonalAccount.UserAuth.MessageListener
 {
-    public class GetUserByIdListener : BaseMessageListener<Guid>
+    public class GetUserByIdListener : BaseMessageListener<GuidRequestDTO>
     {
         public GetUserByIdListener(IServiceProvider serviceProvider, IConfiguration config)
             : base(serviceProvider, config, RabbitQueues.GET_USER_BY_ID) { }
 
         protected override async Task<string?> ProcessMessage(
-            Guid message,
+            GuidRequestDTO message,
             BasicDeliverEventArgs eventArgs,
             IServiceProvider serviceProvider)
         {
             var userRep = serviceProvider.GetRequiredService<IUserRepository>();
 
-            var userData = await userRep.GetUserById(message);
+            var userData = await userRep.GetUserById(message.Id);
 
             return JsonSerializer.Serialize(userData);
         }
