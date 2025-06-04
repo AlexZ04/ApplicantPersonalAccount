ed46935f67e11996896c1c4ef422ece9f7a48da9
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-05 04:29:47 +0700

    [Applicant] finish empty staff controller methods

--- ApplicantPersonalAccount/ApplicantPersonalAccount.Applicant/Controllers/Staff/Main.cs
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.Applicant/Controllers/Staff/Main.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApplicantPersonalAccount.Infrastructure.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace ApplicantPersonalAccount.Applicant.Controllers.Staff
 {
@@ -7,8 +10,43 @@ namespace ApplicantPersonalAccount.Applicant.Controllers.Staff
     [ApiController]
     public partial class StaffController : ControllerBase
     {
-        [HttpGet("test")]
-        public IActionResult BB()
+        [HttpGet("program/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> GetProgramInfo()
+        {
+            return Ok();
+        }
+
+        [HttpGet("application/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> GetApplicationInfo([Required, FromRoute] Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("application/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> EditApplicationInfo([Required, FromRoute] Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("application/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> DeleteApplication([Required, FromRoute] Guid id)
+        public async Task<IActionResult> DeleteApplication([Required, FromRoute] Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("application/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> UpdateApplicationStatus([Required, FromRoute] Guid id)
         {
             return Ok();
         }
