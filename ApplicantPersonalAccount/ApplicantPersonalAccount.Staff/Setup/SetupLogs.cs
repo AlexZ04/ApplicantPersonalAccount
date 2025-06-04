cbcb0047ffb57241200e8696ba1ad0cb0666ff1a
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-04 23:55:31 +0700

    fix logs configuration

--- ApplicantPersonalAccount/ApplicantPersonalAccount.Staff/Setup/SetupLogs.cs
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.Staff/Setup/SetupLogs.cs
@@ -6,8 +6,19 @@ namespace ApplicantPersonalAccount.Staff.Setup
     {
         public static void AddLogs(WebApplicationBuilder builder)
         {
-            builder.Host.UseSerilog((context, configuration)
-                => configuration.ReadFrom.Configuration(context.Configuration));
+            var logDirectory = Path.Combine("../../Logs", DateTime.UtcNow.ToString("yyyy-MM-dd"));
+            Directory.CreateDirectory(logDirectory);
+
+            builder.Host.UseSerilog((context, configuration) =>
+            {
+                configuration
+                    .MinimumLevel.Information()
+                    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(
+                        Path.Combine(logDirectory, "admin_panel-log-.txt"),
+                        rollingInterval: RollingInterval.Day);
+            });
         }
 
         public static void ApplyLogs(WebApplication app)
