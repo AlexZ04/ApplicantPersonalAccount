bd44c3259f72249b239675466c4aa262b74100a2
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-05 03:48:42 +0700

    [Document] add staff controller

---
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.File/Controllers/StaffController.cs
@@ -0,0 +1,96 @@
+﻿using ApplicantPersonalAccount.Common.Enums;
+using ApplicantPersonalAccount.Common.Models.Document;
+using ApplicantPersonalAccount.Document.Services;
+using ApplicantPersonalAccount.Infrastructure.Filters;
+using ApplicantPersonalAccount.Infrastructure.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicantPersonalAccount.Document.Controllers
+{
+    [Route("api/staff")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public StaffController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpPost("file/{userId}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> UploadFileToApplicant(
+            [Required, FromRoute] Guid userId,
+            [FromQuery, Required] FileDocumentType documentType,
+            IFormFile file)
+        {
+            var validationErrors = FileControllerValidator.ValidateFile(file);
+
+            if (validationErrors.Count() > 0)
+                return BadRequest(new { Errors = validationErrors });
+
+            await _fileService.UploadFile(documentType, file, userId);
+
+            return Ok();
+        }
+
+        [HttpDelete("file/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> DeleteApplicantFile([Required, FromRoute] Guid id)
+        {
+            await _fileService.DeleteFile(id, UserDescriptor.GetUserId(User));
+
+            return Ok();
+        }
+
+        [HttpGet("files/{userId}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> GetUserFiles([Required, FromRoute] Guid userId, 
+            [FromQuery, Required] FileDocumentType documentType)
+        {
+            return Ok(await _fileService.GetUserDocuments(documentType, userId));
+        }
+
+        [HttpGet("download/{id}")]
+        [Authorize(Roles = "Manager,HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> DownloadFile([FromRoute] Guid id)
+        {
+            var fileInfo = await _fileService.GetFile(id);
+
+            return File(fileInfo.FileContents, fileInfo.ContentType, fileInfo.FileDownloadName);
+        }
+
+        [HttpPut("passport/{userId}")]
+        [Authorize]
+        [CheckToken]
+        public async Task<IActionResult> EditPassportInfo([Required, FromRoute] Guid userId, 
+            [FromBody] PassportInfoEditModel passport)
+        {
+            await _fileService.EditPassport(passport, userId,
+                UserDescriptor.GetUserRole(User));
+
+            return Ok();
+        }
+
+        [HttpPut("education/{userId}/{documentId}")]
+        [Authorize]
+        [CheckToken]
+        public async Task<IActionResult> EditEducationInfo([Required, FromRoute] Guid userId,
+            [FromBody] EducationInfoEditModel education,
+            [Required, FromRoute] Guid documentId)
+        {
+            await _fileService.EditEducational(education, documentId,
+                userId, UserDescriptor.GetUserRole(User));
+
+            return Ok();
+        }
+    }
+}
