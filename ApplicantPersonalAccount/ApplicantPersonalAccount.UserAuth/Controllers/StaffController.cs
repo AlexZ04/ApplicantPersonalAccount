d11178932046b326cc830075a65cdbcca0e6c9f7
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-05 01:54:49 +0700

    create staff controller

---
+++ ApplicantPersonalAccount/ApplicantPersonalAccount.UserAuth/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using ApplicantPersonalAccount.Infrastructure.Filters;
+using ApplicantPersonalAccount.UserAuth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicantPersonalAccount.UserAuth.Controllers
+{
+    [Route("api/staff")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IManagerService _managerService;
+
+        public StaffController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+        [HttpGet("managers")]
+        [Authorize(Roles = "HeadManager,Admin")]
+        [CheckToken]
+        public async Task<IActionResult> GetManagersList()
+        {
+            return Ok(await _managerService.GetAllManagers());
+        }
+    }
+}
